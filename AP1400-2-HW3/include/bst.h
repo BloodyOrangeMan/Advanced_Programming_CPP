5000da8a86aa16a97163d4aec5461442c5baa387
BloodyOrangeMan <youngrich527@gmail.com>
2023-03-10 18:04:40 +0800

    hw3 done

--- AP1400-2-HW3/include/bst.h
+++ AP1400-2-HW3/include/bst.h
@@ -15,13 +15,14 @@ public:
     BST();
     BST(const BST& bst);
     BST(BST&& bst);
+    ~BST();
 
     Node *&get_root();
     void bfs(std::function<void(Node *&node)> func) const;
     size_t length() const;
     bool add_node(int value);
     Node **find_node(int value);
-    Node **find_parrent(int value);
+    Node **find_parent(int value);
     Node **find_successor(int value);
     bool delete_node(int value);
     
@@ -37,8 +38,13 @@ public:
     Node(int value, Node *left, Node *right);
     Node();
     Node(const Node &node);
+    Node(Node &&node) noexcept;
+
     std::partial_ordering operator<=>(const int &_value) const { return value <=> _value; }
     bool operator==(const int &_value) const { return value == _value; }
+    Node& operator=(const Node &node);
+    Node& operator=(Node &&node) noexcept;
+
 
     friend std::ostream &operator<<(std::ostream &os, const BST::Node &node);
     int value;
--- AP1400-2-HW3/src/bst.cpp
+++ AP1400-2-HW3/src/bst.cpp
@@ -1,6 +1,7 @@
 #include "bst.h"
 
-BST::Node::Node(int value, Node *left, Node *right) : value(value), left(left), right(right)
+BST::Node::Node(int value, Node *left, Node *right)
+    : value(value), left(left), right(right)
 {
 }
 
@@ -17,17 +18,56 @@ std::ostream &operator<<(std::ostream &os, const BST::Node &node)
 }
 BST::Node::Node() : value(0), left(nullptr), right(nullptr) {}
 
-BST::Node::Node(const Node &node) : value(node.value), left(node.left), right(node.right)
+BST::Node::Node(const Node &node)
+    : value(node.value), left(node.left), right(node.right)
 {
+}
+
+BST::Node::Node(Node &&node) noexcept : value(node.value), left(node.left), right(node.right)
+{
+    node.left = nullptr;
+    node.right = nullptr;
+    node.value = 0;
+}
+BST::Node &BST::Node::operator=(Node &&node) noexcept
+{
+    if (this != &node)
+    {
+        value = node.value;
+        left = node.left;
+        right = node.right;
+        node.right = nullptr;
+        node.left = nullptr;
+        node.value = 0;
+    }
+    return *this;
+}
+
+BST::Node &BST::Node::operator=(const BST::Node &node)
+{
+    if (this != &node)
+    {
+        value = node.value;
+        left = node.left;
+        right = node.right;
+    }
+
+    return *this;
 }
 
 BST::BST() : root(nullptr) {}
 
 BST::BST(const BST &bst) : root(bst.root) {}
 
-BST::BST(BST &&bst) : root(bst.root)
+BST::BST(BST &&bst) : root(bst.root) { bst.root = nullptr; }
+
+BST::~BST()
 {
-    bst.root = nullptr;
+    std::vector<Node *> nodes;
+    bfs([&nodes](BST::Node *&node)
+        { nodes.push_back(node); });
+    for (auto &node : nodes)
+        delete node;
 }
 
 BST::Node *&BST::get_root()
@@ -61,9 +101,7 @@ size_t BST::length() const
 {
     size_t length = 0;
     auto count = [&length](Node *&node)
-    {
-        length++;
-    };
+    { length++; };
     bfs(count);
     return length;
 }
@@ -99,14 +137,16 @@ bool BST::add_node(int value)
 
 std::ostream &operator<<(std::ostream &os, const BST &bst)
 {
-    os << "********************************************************************************" << std::endl;
+    os << "*********************************************************************"
+          "***********"
+       << std::endl;
     auto print_bst = [&os](BST::Node *&node)
-    {
-        os << *node << std::endl;
-    };
+    { os << *node << std::endl; };
     bst.bfs(print_bst);
     os << "binary search tree size :" << bst.length() << std::endl;
-    os << "********************************************************************************" << std::endl;
+    os << "*********************************************************************"
+          "***********"
+       << std::endl;
     return os;
 }
 
@@ -154,23 +194,23 @@ BST::Node **BST::find_node(int value)
     // return find_node_helper(root, value);
 }
 
-BST::Node **BST::find_parrent(int value)
+BST::Node **BST::find_parent(int value)
 {
-    Node* node = root;
-    Node** parent = new Node*;
+    Node *node = root;
+    Node **parent = new Node *;
     while (node)
-    {  
+    {
         if (node->value == value)
-        {   
+        {
             return parent;
         }
         else if (node->value < value)
-        {   
+        {
             *parent = node;
             node = node->right;
         }
         else
-        {   
+        {
             *parent = node;
             node = node->left;
         }
@@ -178,3 +218,112 @@ BST::Node **BST::find_parrent(int value)
     return nullptr;
 }
 
+BST::Node **BST::find_successor(int value)
+{
+    Node **node_ptr{find_node(value)};
+    if (!node_ptr)
+        return nullptr;
+    if ((*node_ptr)->right)
+    {
+        *node_ptr = (*node_ptr)->right;
+        while ((*node_ptr)->left)
+        {
+            *node_ptr = (*node_ptr)->left;
+        }
+        Node **p = new Node *;
+        *p = *node_ptr;
+        return p;
+    }
+    else
+    {
+        Node *curr = root;
+        Node *succ = nullptr;
+        while (curr->value != value)
+        {
+            if (curr->value > value)
+            {
+                succ = curr;
+                curr = curr->left;
+            }
+            else
+            {
+                curr = curr->right;
+            }
+        }
+
+        Node **p = new Node *;
+        *p = succ;
+        return p;
+    }
+}
+
+bool BST::delete_node(int value)
+{
+    Node *node = root;
+    Node **parent = new Node *;
+    while (node)
+    {
+        if (node->value == value)
+        {
+            if (node->left && node->right)
+            {
+                Node **succ = find_successor(value);
+                std::cout << **succ << std::endl;
+                int temp = (*succ)->value;
+                delete_node((*succ)->value);
+                node->value = temp;
+                std::cout << *node << std::endl;
+            }
+            else if (node->left || node->right)
+            {
+                if ((*parent)->value < value)
+                {
+                    if (node->left)
+                    {
+                        (*parent)->right = node->left;
+                    }
+                    else
+                    {
+                        (*parent)->right = node->right;
+                    }
+                }
+                else
+                {
+                    if (node->left)
+                    {
+                        (*parent)->left = node->left;
+                    }
+                    else
+                    {
+                        (*parent)->left = node->right;
+                    }
+                }
+                delete node;
+            }
+            else
+            {
+                if ((*parent)->value > value)
+                {
+                    (*parent)->left = nullptr;
+                }
+                else
+                {
+                    (*parent)->right = nullptr;
+                }
+                delete node;
+            }
+            return true;
+        }
+        else if (node->value < value)
+        {
+            *parent = node;
+            node = node->right;
+        }
+        else
+        {
+            *parent = node;
+            node = node->left;
+        }
+    }
+    return false;
+}
